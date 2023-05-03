181c76f86644d9c793f917092eecc4260d67939f
omza10 <omrizach12@gmail.com>
2023-05-04 00:59:57 +0300

    Changed the quiz layout

--- app/src/main/java/com/gefilte/app/MainActivity.java
+++ app/src/main/java/com/gefilte/app/MainActivity.java
@@ -5,23 +5,28 @@ import androidx.appcompat.app.AppCompatDelegate;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button button;
+    Button register_btn,login_btn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
         setContentView(R.layout.activity_main);
-        button = findViewById(R.id.login);
-        button.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                Intent intent = new Intent(getApplicationContext(), HomeScreen.class);
+        login_btn = findViewById(R.id.login);
+        register_btn = findViewById(R.id.register);
+        login_btn.setOnClickListener(v -> {
+                Intent intent = new Intent(getApplicationContext(), login.class);
                 startActivity(intent);
             }
-        });
+        );
+
+        register_btn.setOnClickListener(v -> {
+            Intent intent = new Intent(getApplicationContext(), Register.class);
+            startActivity(intent);
+            }
+        );
     }
 }
